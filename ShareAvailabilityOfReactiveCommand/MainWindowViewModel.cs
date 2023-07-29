92416dca2fec1a84685e00d1d53fb93af0eab24f
ShibamiyaRyobee <140879540+ShibamiyaRyobee@users.noreply.github.com>
2023-07-29 19:02:46 +0900

    再現コードを作成

---
+++ ShareAvailabilityOfReactiveCommand/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ShareAvailabilityOfReactiveCommand
+{
+    public class MainWindowViewModel
+    {
+        public ReadOnlyReactivePropertySlim<bool> CanMouseDown { get; }
+
+        public ReactiveCommand<bool> ChangeModeCommand { get; }
+
+        public ReactiveCommandSlim<MouseEventArgs> MouseMoveCommand { get; }
+
+        public ReactiveCommandSlim<MouseEventArgs> MouseDownCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            ChangeModeCommand = new ReactiveCommand<bool>();
+            CanMouseDown = ChangeModeCommand.ToReadOnlyReactivePropertySlim();
+            CanMouseDown.Subscribe(x => Console.WriteLine($"Click {(x ? "Enabled" : "Disabled")}."));
+            MouseMoveCommand = new ReactiveCommandSlim<MouseEventArgs>().WithSubscribe(_ => Console.WriteLine("Moved."));
+            MouseDownCommand = CanMouseDown.ToReactiveCommandSlim<MouseEventArgs>().WithSubscribe(_ => Console.WriteLine("Clicked."));
+        }
+    }
+}
